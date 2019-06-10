7e1e86135f3b59b2540d2c1871f35a804aafa617
katsu tanabe <katsu1@live.com>
2019-06-09 22:10:51 -0600

    intital stage of html,javascrip and jquery

---
+++ assets/javascript/game.js
@@ -0,0 +1,50 @@
+//global variables
+var crystal = {
+    blue: {
+        name: "Blue",
+        value: 0
+    },
+    green: {
+        name: "Green",
+        value: 0
+        
+    },
+    red: {
+        name: "Red",
+        value: 0
+        
+    },
+    yellow: {
+        name: "Yellow",
+        value: 0
+        
+    }
+    
+};
+//scores
+var currentScore = 0;
+var tergetScore = 0;
+//wins and losses
+var winCount = 0;
+var lossCount = 0;
+
+
+//Fucntions
+$("#blue").click(function() {
+    alert("test");
+});
+$("#red").click(function() {
+    alert("test");
+});
+$("#green").click(function() {
+    alert("test");
+});
+$("#yellow").click(function() {
+    alert("test");
+});
+
+
+
+//main process
+
+
